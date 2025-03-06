b67b286e50e750fd76571c4a4e82146ff8b6ec9b
nanfengovo <nanfengovo@gmail.com>
2025-03-06 22:51:41 +0800

    配置系统读取01

---
+++ Read Configuration01/Read Configuration01/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+
+await Task.Run(async () =>
+{
+    while (true)
+    {
+        ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.AddJsonFile("config.json", optional: false, reloadOnChange: true);
+        IConfigurationRoot configurationRoot = configurationBuilder.Build();
+        string name = configurationRoot["name"];
+        Console.WriteLine($"name = {name}");
+        string proxyAddress = configurationRoot.GetSection("proxy:address").Value;
+        Console.WriteLine($"address={proxyAddress}");
+        await Task.Delay(2000);
+    }
+});
+
+
